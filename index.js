5bac1e4c710caf17e9235f684219f2d2e19142a3
Nikolay Karavaev <n-karavaev@yandex.ru>
2020-07-14 17:32:17 +0300

    4th work second try v1

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const editProfileBtn = document.querySelector(".profile__edit_button");
-const closePopUpBtn = document.querySelector(".popup__close-button");
-const popUp = document.querySelector(".popup");
+let editProfileBtn = document.querySelector(".profile__edit_button");
+let closePopUpBtn = document.querySelector(".popup__close-button");
+let popUp = document.querySelector(".popup");
 let formElement = document.querySelector(".popup__container");
-let nameInput = document.querySelector(".popup__input1");
-let bioInput = document.querySelector(".popup__input2");
-let profileName = document.querySelector(".profile__line1");
-let profileBio = document.querySelector(".profile__line2");
+let nameInput = document.querySelector(".popup__input-one");
+let bioInput = document.querySelector(".popup__input-two");
+let profileName = document.querySelector(".profile__line-one");
+let profileBio = document.querySelector(".profile__line-two");
 
 
 function openPopUp() {
-    popUp.classList.add("popup__is-open");
+    popUp.classList.add("popup_is-open");
     nameInput.value = profileName.textContent;
     bioInput.value = profileBio.textContent;
 }
 
 function closePopUp() {
-    popUp.classList.remove("popup__is-open");
+    popUp.classList.remove("popup_is-open");
 }
 editProfileBtn.addEventListener("click", openPopUp);
 closePopUpBtn.addEventListener("click", closePopUp);
